6f343d9b3b0216352c1637ef3c22dff487f4fb7b
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2024-10-08 15:50:51 +0200

    add a new div for flag verification

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-function nextLevel(id) {
-  alert("Are you ready for the next level?");
+function nextLevel() {
+  const level_verify = document.createElement('div');
+  level_verify.className = 'level_verify';
+  level_verify.textContent = 'I am the new div';
+  document.getElementById('verify').appendChild(level_verify);
 }
 
 function explainer() {
-  document.getElementById("verify").innerHTML = "Hey there";
+  document.getElementById('verify').innerHTML = "Hello I am The Flag";
 }
